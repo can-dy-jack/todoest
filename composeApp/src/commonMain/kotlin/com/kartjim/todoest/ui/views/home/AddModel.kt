adfa09657c06d749de64ebe5794d7ef08d38b878
kart jim <jjack_chen3@163.com>
2025-07-27 16:18:45 +0800

    优化新增弹窗，增加Priority选择项

--- composeApp/src/commonMain/kotlin/com/kartjim/todoest/ui/views/home/AddModel.kt
+++ composeApp/src/commonMain/kotlin/com/kartjim/todoest/ui/views/home/AddModel.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,12 +18,18 @@ import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Flag
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -50,9 +57,11 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kartjim.todoest.data.Priority
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.number
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
@@ -86,6 +95,7 @@ fun AddModel(
         ) {
             val title = rememberTextFieldState()
             val description = rememberTextFieldState()
+            var priority = remember { mutableStateOf(Priority.NOT_EMERGENCY_NOT_IMPORTANT) }
             val startTime = remember { mutableLongStateOf(0) }
             val endTime = remember { mutableLongStateOf(0) }
             val focusRequester = remember { FocusRequester() }
@@ -115,7 +125,7 @@ fun AddModel(
                     colors = TextFieldDefaults.colors(
                         unfocusedContainerColor = Color.Transparent,
                         focusedContainerColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Gray,
+                        unfocusedIndicatorColor = Color.Transparent,
 //                        focusedIndicatorColor = Color.Green
                     ),
                     contentPadding = PaddingValues(8.dp),
@@ -145,18 +155,16 @@ fun AddModel(
                     )
                 )
 
-                Row(
+                Row (
                     modifier = Modifier
-                        .padding(5.dp)
                         .fillMaxWidth(),
 //                    contentAlignment = Alignment.CenterEnd
                 ) {
                     val showStartTimeDialog = remember { mutableIntStateOf(0) }
-                    OutlinedButton(
+                    TextButton(
                         onClick = {
                             showStartTimeDialog.value = 1
                         },
-                        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 5.dp),
 //                        modifier = Modifier.hoverable()
                     ) {
                         if (startTime.value == 0L)
@@ -170,11 +178,10 @@ fun AddModel(
                             )
 
                     }
-                    OutlinedButton(
+                    TextButton(
                         onClick = {
                             showStartTimeDialog.value = 2
                         },
-                        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 5.dp),
                         modifier = Modifier.padding(horizontal = 5.dp)
                     ) {
                         if (endTime.value == 0L)
@@ -207,12 +214,66 @@ fun AddModel(
 
 
                 }
-                Box(
+                Row(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    contentAlignment = Alignment.CenterEnd
                 ) {
-                    Button(
+                    var expanded by remember { mutableStateOf(false) }
+                    Box() {
+                        IconButton(
+                            onClick = { expanded = !expanded }
+                        ) {
+                            Icon(
+                                Icons.Default.Flag,
+                                contentDescription = "选择优先级",
+                                tint =
+                                    when (priority.value) {
+                                        Priority.NOT_EMERGENCY_IMPORTANT -> Color.Magenta
+                                        Priority.EMERGENCY_IMPORTANT -> Color.Red
+                                        Priority.EMERGENCY_NOT_IMPORTANT -> Color.Yellow
+                                        Priority.NOT_EMERGENCY_NOT_IMPORTANT -> Color.Gray
+                                    }
+
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            DropdownMenuItem(
+                                text = { Text("不重要不紧急", color = Color.Gray) },
+                                onClick = {
+                                    priority.value = Priority.NOT_EMERGENCY_NOT_IMPORTANT
+                                    expanded = false
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = { Text("重要但不紧急", color =  Color.Magenta) },
+                                onClick = {
+                                    priority.value = Priority.NOT_EMERGENCY_IMPORTANT
+                                    expanded = false
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = { Text("紧急但不重要", color = Color.Yellow) },
+                                onClick = {
+                                    priority.value = Priority.EMERGENCY_NOT_IMPORTANT
+                                    expanded = false
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = { Text("重要且紧急", color = Color.Red) },
+                                onClick = {
+                                    priority.value = Priority.EMERGENCY_IMPORTANT
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    FilledTonalButton(
                         onClick = {
                             if (!title.text.isEmpty()) {
                                 viewModel.addTodo(
@@ -264,11 +325,15 @@ fun DatePickerModal(
 @OptIn(ExperimentalTime::class)
 @Composable
 fun TimestampDisplay(timestamp: Long, text: (date: String) -> String) {
+    val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault());
     // 假设时间戳是毫秒
     val dateString = remember(timestamp) {
         val instant = Instant.fromEpochMilliseconds(timestamp)
         val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
-        "${localDateTime.year}-${localDateTime.month.number.padStart(2)}-${
+
+        val year = if (now.year == localDateTime.year) "" else "${localDateTime.year}-"
+
+        "${year}${localDateTime.month.number.padStart(2)}-${
             localDateTime.day.padStart(
                 2
             )
